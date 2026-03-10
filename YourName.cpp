3a4b942db6e9210966a6d223f099ef454a98c76c
junminhatakeyama <jun.min.hatakeyama@gmail.com>
2026-03-10 08:04:32 -0500

    Update YourName.cpp

--- YourName.cpp
+++ YourName.cpp
@@ -5,5 +5,12 @@ int main() {
     // Create your 'Your Name' program here.
     // Your program should ask the user for their name and then output
     // "Your name is " followed by the user's name.
+
+    std::string name;
+    // Get username (name)
+    std::cout << "Enter your username: ";
+    std::getline(std::cin, name);
+    
+    std::cout << "Your name is " << name << std::endl;
     
 }
